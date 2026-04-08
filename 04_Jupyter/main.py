f98bcbda795d93a648c2244e1c663d3398a51b19
RoshanGurung-Eng <roshantheaomine@gmail.com>
2026-04-08 19:40:23 +0545

    Deleted duplicates

--- 04_Jupyter/main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from 04-jupyter!")
-
-
-if __name__ == "__main__":
-    main()
--- 10_MUltiple _PolynomialRegression/main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from 10-multiple-polynomialregression!")
-
-
-if __name__ == "__main__":
-    main()
